fb5e9fa07ea19c0b32ac2f349780a989eb847cc2
UsernameDiana <ms.strele@gmail.com>
2017-04-20 13:12:18 +0200

    test

--- AirlineNetwork/src/airportDomain/Category.java
+++ AirlineNetwork/src/airportDomain/Category.java
@@ -1,5 +1,5 @@
 package airportDomain;
-// setting up passenger constant categories using enum
+// setting up constant categories using enum
 public enum Category {
   LateToFlight,
   BusinessClass,
