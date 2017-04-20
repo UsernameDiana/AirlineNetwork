8e097972f4db84f6653446c80fc882fc1c58c3d2
stinaanita <stina-stud@live.dk>
2017-04-20 12:25:12 +0200

    text files

---
+++ AirlineNetwork/src/airports/Reader.java
@@ -0,0 +1,22 @@
+package airports;
+
+import java.io.IOException;
+//import utils.FileUtility;
+
+public class Reader 
+{
+    public static void main(String[] args) throws IOException 
+    {
+     //   String[] words = FileUtility.toStringArray("src\\main\\java\\airports\\airports.txt", "[^A-Za-z]");
+//
+//        for (String word:words)
+//        {
+//            if (word.equals("airport"))
+//            {
+//                System.out.println(word);
+//        
+//            }
+//        }
+    }
+
+}
