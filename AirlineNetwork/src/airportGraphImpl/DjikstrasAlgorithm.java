04681d6b871c4e90af11f428ee6b9a71e73fe474
UsernameDiana <ms.strele@gmail.com>
2017-04-28 10:18:03 +0200

    Starting on DjikstrasAlgorithm

--- AirlineNetwork/src/airportGraphImpl/DjikstrasAlgorithm.java
+++ AirlineNetwork/src/airportGraphImpl/DjikstrasAlgorithm.java
@@ -1,8 +1,22 @@
 package airportGraphImpl;
 
+import airportDomain.Airport;
+import airportDomain.Route;
+import java.util.List;
+import java.util.Map;
 
 // 2. Find shortest path (distance) from one location to another
-
 public class DjikstrasAlgorithm {
+
+    private final List<Airport> vertices;
+    private final List<Route> edges;
+    private List<Airport> visited;
+    private List<Airport> notVisited;
+    private Map<Airport, Route> distance;
     
+    public DjikstrasAlgorithm(List<Airport> vertices, List<Route> edges) {
+        this.vertices = vertices;
+        this.edges = edges;
+    }
+
 }
